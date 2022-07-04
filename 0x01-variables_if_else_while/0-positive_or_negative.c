08bd2b68bdd08de709e6959f8a3bd1814a1a9410
Emmanuelddf <eigbadonemmanuel@gmail.com>
2022-07-04 14:40:46 -0700

    positive or negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Positive or negative - print sign of random number
+ * @void: Empty parameter list for main.
+ *
+ * Description: this if/else statement declares the sign (positive
+ * or negative) of a random number. it also states if the value
+ * is zero.
+ * Return: 0 if exited properly, non-zero if otherwise
+ */
+int main(void)
+{
+	/**
+	 * variable n has been declared
+	 * coding then assigns the random value
+	 */
+
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
+
+	return (0);
+}
